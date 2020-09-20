f7f4f44f9ee386b792d949a55b1630106494a30e
Loki Astari <Loki.Astari@gmail.com>
2020-09-19 17:27:29 -0700

    Add multiple exception types

--- src/ThorsLogging/ThorsLogging.h
+++ src/ThorsLogging/ThorsLogging.h
@@ -13,7 +13,33 @@
 
 namespace ThorsAnvil::Logging
 {
+    // This is for data/application corruption that requires the
+    // application to immediately quit.
+    class FatalException: std::runtime_error
+    {
+        public:
+            using std::runtime_error::runtime_error;
+    };
+    // This is for situations that "should not" happen.
+    // Use this exception for code paths that should not be reachable.
+    //
+    // This will help find issues that can be corrected by unit tests.
+    // But occasionally will happen in production. You should only
+    // catch this type of error at an action type level to abandon the
+    // action.
+    //
+    // Note: This does not mean there has been data corruptions so we
+    //       can potentially continue. The application writer must
+    //       make that determination.
     class CriticalException: std::runtime_error
+    {
+        public:
+            using std::runtime_error::runtime_error;
+    };
+    // This is for situations where you don't want to happen
+    // but is caused because the input data does not conform
+    // to expect patterns.
+    class LogicalException: std::runtime_error
     {
         public:
             using std::runtime_error::runtime_error;
@@ -32,8 +58,10 @@ do                                                                      \
 }                                                                       \
 while (false)
 
-#define ThorsLogAndThrow(...)          ThorsLogAndThrowAction(2, std::runtime_error, __VA_ARGS__)
-#define ThorsLogAndThrowCritical(...)  ThorsLogAndThrowAction(ERROR, ThorsAnvil::Logging::CriticalException, __VA_ARGS__)
+#define ThorsLogAndThrowFatal(...)      ThorsLogAndThrowAction(FATAL,   ThorsAnvil::Logging::FatalException, __VA_ARGS__)
+#define ThorsLogAndThrowCritical(...)   ThorsLogAndThrowAction(ERROR,   ThorsAnvil::Logging::CriticalException, __VA_ARGS__)
+#define ThorsLogAndThrowLogical(...)    ThorsLogAndThrowAction(WARNING, ThorsAnvil::Logging::LogicalException, __VA_ARGS__)
+#define ThorsLogAndThrow(...)           ThorsLogAndThrowAction(2, std::runtime_error, __VA_ARGS__)
 
 
 #endif
