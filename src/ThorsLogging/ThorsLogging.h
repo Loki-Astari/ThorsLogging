eb06d0398803ef9c4216496c766706c15642d29b
Loki Astari <Loki.Astari@gmail.com>
2023-08-08 08:39:21 -0700

    Fix

--- src/ThorsLogging/ThorsLogging.h
+++ src/ThorsLogging/ThorsLogging.h
@@ -10,40 +10,41 @@
 
 #if defined(THORS_LOGGING_HEADER_ONLY) && THORS_LOGGING_HEADER_ONLY == 1
 
-namespace loguru {
-	enum NamedVerbosity : Verbosity
-	{
-		// Used to mark an invalid verbosity. Do not log to this level.
-		Verbosity_INVALID = -10, // Never do LOG_F(INVALID)
+namespace loguru
+{
+    enum NamedVerbosity : Verbosity
+    {
+        // Used to mark an invalid verbosity. Do not log to this level.
+        Verbosity_INVALID = -10, // Never do LOG_F(INVALID)
 
-		// You may use Verbosity_OFF on g_stderr_verbosity, but for nothing else!
-		Verbosity_OFF     = -9, // Never do LOG_F(OFF)
+        // You may use Verbosity_OFF on g_stderr_verbosity, but for nothing else!
+        Verbosity_OFF     = -9, // Never do LOG_F(OFF)
 
-		// Prefer to use ABORT_F or ABORT_S over LOG_F(FATAL) or LOG_S(FATAL).
-		Verbosity_FATAL   = -3,
-		Verbosity_ERROR   = -2,
-		Verbosity_WARNING = -1,
+        // Prefer to use ABORT_F or ABORT_S over LOG_F(FATAL) or LOG_S(FATAL).
+        Verbosity_FATAL   = -3,
+        Verbosity_ERROR   = -2,
+        Verbosity_WARNING = -1,
 
-		// Normal messages. By default written to stderr.
-		Verbosity_INFO    =  0,
+        // Normal messages. By default written to stderr.
+        Verbosity_INFO    =  0,
 
-		// Same as Verbosity_INFO in every way.
-		Verbosity_0       =  0,
+        // Same as Verbosity_INFO in every way.
+        Verbosity_0       =  0,
 
-		// Verbosity levels 1-9 are generally not written to stderr, but are written to file.
-		Verbosity_1       = +1,
-		Verbosity_2       = +2,
-		Verbosity_3       = +3,
-		Verbosity_4       = +4,
-		Verbosity_5       = +5,
-		Verbosity_6       = +6,
-		Verbosity_7       = +7,
-		Verbosity_8       = +8,
-		Verbosity_9       = +9,
+        // Verbosity levels 1-9 are generally not written to stderr, but are written to file.
+        Verbosity_1       = +1,
+        Verbosity_2       = +2,
+        Verbosity_3       = +3,
+        Verbosity_4       = +4,
+        Verbosity_5       = +5,
+        Verbosity_6       = +6,
+        Verbosity_7       = +7,
+        Verbosity_8       = +8,
+        Verbosity_9       = +9,
 
-		// Do not use higher verbosity levels, as that will make grepping log files harder.
-		Verbosity_MAX     = +9,
-	};
+        // Do not use higher verbosity levels, as that will make grepping log files harder.
+        Verbosity_MAX     = +9,
+    };
 }
 
 #ifndef THOR_LOGGING_DEFAULT_LOG_LEVEL
